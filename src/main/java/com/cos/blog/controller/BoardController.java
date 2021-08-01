fec6a31976bdc5c48c01d7cad1d7d3c07582c8f1
pung <positionace@naver.com>
2021-08-01 18:44:28 +0900

    upgrade 빼고 완료

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
@@ -24,6 +25,17 @@ public class BoardController {
 		return "index"; //viewResolver 작동
 	}
 	
+	@GetMapping("/board/{id}")
+	public String FindById(@PathVariable int id,Model model) {
+		model.addAttribute("board",boardService.글상세보기(id));
+		return "board/detail";
+	}
+	
+	@GetMapping("/board/{id}/updateForm")
+	public String updateForm(@PathVariable int id,Model model) {
+		model.addAttribute("board",boardService.글상세보기(id));
+		return "board/updateForm";
+	}
 	//USER 권한이 필요
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,12 @@ let index ={
 	  $("#btn-save").on("click",()=>{ // function(){} 안쓰고 , ()=>{} 쓴 이유는 this를 바인딩하기 위해서
 		this.save();
 	});
+	  $("#btn-delete").on("click",()=>{ // function(){} 안쓰고 , ()=>{} 쓴 이유는 this를 바인딩하기 위해서
+		this.deleteById();
+	});
+	  $("#btn-update").on("click",()=>{ // function(){} 안쓰고 , ()=>{} 쓴 이유는 this를 바인딩하기 위해서
+		this.update();
+	});
 	 /*$("#btn-login").on("click",()=>{ // function(){} 안쓰고 , ()=>{} 쓴 이유는 this를 바인딩하기 위해서
 		this.login();
 	 });*/		
@@ -34,7 +40,23 @@ let index ={
 		}); 
 		
 	},*/
-save: function(){
+deleteById: function(){
+       let id=$("#id").text();     
+
+		$.ajax({
+			type:"DELETE",
+			url:"/api/board/"+id,
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면)=>javascript오브젝트로 변경해줌
+			
+		}).done(function(resp){
+			alert("삭제가 완료되었습니다.");
+			location.href ="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); 
+		
+	},
+	save: function(){
 		//alert('user의 save함수 호출됨');
 		let data ={
 			title: $("#title").val(),
@@ -56,7 +78,31 @@ save: function(){
 			alert(JSON.stringify(error));
 		}); 
 		
-	}	
+	},	
+	update: function(){
+		let id = $("#id").val();
+		
+		let data ={
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+		
+
+		$.ajax({
+			type:"PUT",
+			url:"/api/board/"+id,
+			data: JSON.stringify(data), // http body데이터
+			contentType: "application/json; charset=utf-8", //body 데이터가 어떤 타입인지
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면)=>javascript오브젝트로 변경해줌
+			
+		}).done(function(resp){
+			alert("글수정이 완료되었습니다.");
+			location.href ="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); 
+		
+	},			
 }
 
 index.init();
